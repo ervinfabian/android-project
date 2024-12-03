6cfe87159cfb5968e83903f91be53b5fd84312be
ervinfabian <ervinfab@gmail.com>
2024-12-03 23:55:41 +0200

    harmadik android

--- android-project/app/src/main/java/com/tasty/recipesapp/repository/recipe/RecipeRepository.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/repository/recipe/RecipeRepository.kt
@@ -1,30 +1,68 @@
 package com.tasty.recipesapp.repository.recipe
 
+import android.content.Context
+import android.util.Log
+import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.reflect.TypeToken
+import com.tasty.recipesapp.repository.recipe.model.RecipeDTO
+import com.tasty.recipesapp.repository.recipe.model.RecipeModel
+import com.tasty.recipesapp.repository.recipe.model.toModel
 import java.io.IOException
 
 // ebbe kell a gson-os cucc
-class RecipeRepository (
-//    fun getRecipesFromJson(context: Context): List<RecipeModel> {
-//        lateinit var jsonString: String
-//        try {
-//            jsonString =
-//                context.assets.open("all_recipes.json")
-//                    .bufferedReader()
-//                    .use{it.readText()
-//                    }
-//        }catch (ioException: IOException) {
-//            Log.e(TAG, "Error occurred while reading JSON file: $ioException")
-//        }
-//
-//        val type = object : TypeToken<List<RecipeDTO>>() {}.type
-//        val recipesDTOList: List<RecipeDTO> = Gson().fromJson(jsonString, type)
-//
-//        val recipesList = recipesDTOList.toModelList()
-//        return recipesList
-//    }
+class RecipeRepository {
+
+    private val TAG = "RecipeRepository"
+
+    fun getRecipesFromJson(context: Context): List<RecipeModel> {
+    lateinit var jsonString: String
+    try {
+        jsonString =
+            context.assets.open("all_recipes.json")
+                .bufferedReader()
+                .use{it.readText()
+                }
+    }catch (ioException: IOException) {
+        Log.e(TAG, "Error occurred while reading JSON file: $ioException")
+    }
+
+    val type = object : TypeToken<List<RecipeDTO>>() {}.type
+    val recipesDTOList: List<RecipeDTO> = Gson().fromJson(jsonString, type)
+
+    val recipesList = recipesDTOList.toModelList()
+    return recipesList
+    }
 
 
 
     // list related operations
-)
+}
 
+fun getRecipesFromJson(context: Context): List<RecipeModel> {
+    lateinit var jsonString: String
+    try {
+        jsonString =
+            context.assets.open("all_recipes.json")
+                .bufferedReader()
+                .use{it.readText()
+                }
+    }catch (ioException: IOException) {
+        Log.e(TAG, "Error occurred while reading JSON file: $ioException")
+    }
+
+    val type = object : TypeToken<List<RecipeDTO>>() {}.type
+    val recipesDTOList: List<RecipeDTO> = Gson().fromJson(jsonString, type)
+
+    val recipesList = recipesDTOList.toModelList()
+    return recipesList
+}
+
+fun RecipeDTO.toModel(): RecipeModel{
+    return RecipeModel(
+        id = this.recipeId,
+        name = this.name,
+        description = this.description,
+        thumbnailUrl = this.thumbnailUrl)
+}
+fun List<RecipeDTO>.toModelList(): List<RecipeModel> {
+    return this.map { it.toModel() }
+}
